6db54910df05f50e96ada2a1ca172cc463ff39e8
yosakiva <jakkapan_longlai@cmu.ac.th>
2025-05-20 18:26:32 +0700

    add Error massage

---
+++ src/components/ErrorMassage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+interface ErrorMassageProps{
+  id: string
+}
+defineProps<ErrorMassageProps>()
+</script>
+<template>
+  <p aria-live="assertive" class="errorMassage" :id="id">
+    <slot />
+  </p>
+</template>
